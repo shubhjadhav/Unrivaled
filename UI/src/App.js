4e26352710f8feac54b1e3eeaf980f82e3e36859
marrywise <maryamtoushehjoo@gmail.com>
2022-11-12 20:15:23 -0500

    Adding Files for landing page

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/common/header.js';
+import Login from './components/landingPage/login.js';
 
 function App() {
   return (
+
     <div className="App">
       <Header/>
     </div>
+
   );
+
 }
+
 
 export default App;
