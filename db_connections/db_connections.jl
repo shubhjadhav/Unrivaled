ea4a59ab07f4d00bbb36a1357a8852af06897359
NahomSeleshi <nahomseleshi43@gmail.com>
2022-11-14 11:52:12 -0500

    The methods get_one_resume(), get_user_resume(), get_one_jd(), and get_user_jd() are implemented.

--- db_connections/db_connections.jl
+++ db_connections/db_connections.jl
@@ -12,6 +12,7 @@ resumes = unrivaled["resumes"]
 job_descriptions = unrivaled["job_descriptions"]
 scores = unrivaled["scores"]
 
+
 # This funciton is used to show the contents of a given database
 function show_db(db_name)
     doc_list = Mongoc.find(db_name)
@@ -21,6 +22,7 @@ function show_db(db_name)
     end
     println()
 end
+
 
 # Create DB Entry
 # This input is a dictionary from the UI but I am using a sample here.
@@ -44,6 +46,7 @@ function file_upload(dictionary)
     println("The document is successfully uploaded.")
 end
 
+
 # This funciton takes signup info and adds it to the user_details collection
 function user_detail_upload(dictionary)
     vector = [Mongoc.BSON(dictionary)]
@@ -52,12 +55,14 @@ function user_detail_upload(dictionary)
 end
 # user_detail_upload(cur_dict)
 
+
 # This funciton takes file upload info and adds it to the resumes collection
 function resume_upload(dictionary)
     vector = [Mongoc.BSON(dictionary)]
     append!(resumes, vector)
     println("The user's resume is successfully uploaded.")
 end
+
 
 # This funciton takes job description and adds it to the job_descriptions collection
 function job_description_upload(dictionary)
@@ -66,12 +71,14 @@ function job_description_upload(dictionary)
     println("The user's job description is successfully uploaded.")
 end
 
+
 # This funciton takes signup info and adds it to the user_details collection
 function score_upload(dictionary)
     vector = [Mongoc.BSON(dictionary)]
     append!(scores, vector)
     println("The user's score is successfully uploaded.")
 end
+
 
 # Read from DB collections
 # This function is used to return a path for a given username and resume/job_description
@@ -87,6 +94,7 @@ function get_path(username, filetype)
         return cur_dict["Name"] *"/"*cur_dict["filename"]*"/"*cur_dict["resume_name"]
     end
 end
+
 
 # This function is used to check if a user is a registered user or not and returns a boolean value
 function is_registered_user(dictionary)
@@ -110,6 +118,7 @@ function is_registered_user(dictionary)
     return registered
 end
 
+
 # This funciton returns all the resumes in the resume collection
 function get_all_resumes()
     resume_list = Mongoc.find(resumes,options=Mongoc.BSON("""{ "projection":{ "_id":false,"file":false }}"""))
@@ -118,6 +127,60 @@ function get_all_resumes()
         push!(a, Mongoc.as_dict(res))
     end
     return a
+end
+
+
+# This function returns one resume using username and resume name 
+# as a key to search in the resume collection
+function get_one_resume(dictionary)
+    username = dictionary["username"]
+    resume_name = dictionary["resume"]
+    cur_file = Mongoc.find(resumes, Mongoc.BSON("""{"Name": "$username", "resume": "$resume_name"}"""))
+    final_resume = []
+    for each in cur_file
+        push!(final_resume, each)
+    end
+    return final_resume
+end
+
+
+# This function returns all the resumes of a specific user using username 
+# as a key to search in the resume collection
+function get_user_resume(dictionary)
+    username = dictionary["username"]
+    cur_file = Mongoc.find(resumes, Mongoc.BSON("""{"Name": "$username"}"""))
+    final_resumes = []
+    for each in cur_file
+        push!(final_resumes, each)
+    end
+    return final_resumes
+end
+
+
+# This function returns one job description using username and jd as a key 
+# to search in the job_descriptions collection
+function get_one_jd(dictionary)
+    username = dictionary["username"]
+    jd_name = dictionary["jd"]
+    cur_file = Mongoc.find(job_descriptions, Mongoc.BSON("""{"Name": "$username", "jd": "$jd_name"}"""))
+    final_jd = []
+    for each in cur_file
+        push!(final_jd, each)
+    end
+    return final_jd
+end
+
+
+# This function returns all the jd's of a specific user using username and 
+# jd name as a key to search in the job_descriptions collection
+function get_user_jd(dictionary)
+    username = dictionary["username"]
+    cur_file = Mongoc.find(job_descriptions, Mongoc.BSON("""{"Name": "$username"}"""))
+    final_jd = []
+    for each in cur_file
+        push!(final_jd, each)
+    end
+    return final_jd
 end
 
 is_registered_user(Dict("Name"=> "Hello", "password" => "1234"))
